9f2dcc1f9b4a5b2c971397003e709b03e14bd0b9
declan.newman <declan.newman@virginholidays.com>
2015-01-21 11:24:07 +0000

    Allow for a specific version of Varnish

--- manifests/init.pp
+++ manifests/init.pp
@@ -52,6 +52,7 @@ class varnish (
   $storage_size = $varnish::params::storage_size,
   $varnish_version = $varnish::params::varnish_version,
   $vcl_reload = $varnish::params::vcl_reload,
+  $specific_version = $varnish::params::specific_version,
   $runtime_params = {}
 ) inherits varnish::params {
 
