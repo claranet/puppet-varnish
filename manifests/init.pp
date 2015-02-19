26291c734d78d81a77aada76a3e824aaa5122f60
Tron Thongsringklee <tron@bashton.com>
2015-02-19 14:10:47 +0000

    Specify version from varnish::package_ensure

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,24 +36,24 @@
 #   Hash of runtime parameters
 #
 class varnish (
-  $addrepo = true,
-  $secret = 'notsosecret',
-  $secret_file = $varnish::params::secret_file,
-  $vcl_conf = $varnish::params::vcl_conf,
-  $listen = $varnish::params::listen,
-  $listen_port = $varnish::params::listen_port,
-  $admin_listen = $varnish::params::admin_listen,
-  $admin_port = $varnish::params::admin_port,
-  $min_threads = $varnish::params::min_threads,
-  $max_threads = $varnish::params::max_threads,
-  $thread_timeout = $varnish::params::thread_timeout,
-  $storage_type = $varnish::params::storage_type,
-  $storage_file = $varnish::params::storage_file,
-  $storage_size = $varnish::params::storage_size,
+  $addrepo         = true,
+  $secret          = 'notsosecret',
+  $secret_file     = $varnish::params::secret_file,
+  $vcl_conf        = $varnish::params::vcl_conf,
+  $listen          = $varnish::params::listen,
+  $listen_port     = $varnish::params::listen_port,
+  $admin_listen    = $varnish::params::admin_listen,
+  $admin_port      = $varnish::params::admin_port,
+  $min_threads     = $varnish::params::min_threads,
+  $max_threads     = $varnish::params::max_threads,
+  $thread_timeout  = $varnish::params::thread_timeout,
+  $storage_type    = $varnish::params::storage_type,
+  $storage_file    = $varnish::params::storage_file,
+  $storage_size    = $varnish::params::storage_size,
   $varnish_version = $varnish::params::varnish_version,
-  $vcl_reload = $varnish::params::vcl_reload,
-  $specific_version = $varnish::params::package_ensure,
-  $runtime_params = {}
+  $vcl_reload      = $varnish::params::vcl_reload,
+  $package_ensure  = $varnish::params::package_ensure,
+  $runtime_params  = {}
 ) inherits varnish::params {
 
   validate_bool($addrepo)
