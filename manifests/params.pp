35ea79b33efe4eb28d83804bce031e11c3db88ae
Doug Neal <doug@neal.me.uk>
2015-03-06 15:30:03 +0000

    Expand params.pp with:  - a Debian osfamily section  - specifics for Ubuntu precise (12.04)  - the template name  - whether a package repository should be installed (addrepo)

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,32 +4,55 @@
 # It sets variables according to platform
 #
 class varnish::params {
+  
+  $package_name = 'varnish'
+  $service_name = 'varnish'
+  
   case $::osfamily {
     'RedHat', 'Amazon': {
-      $package_name = 'varnish'
-      $service_name = 'varnish'
       $vcl_reload   = '/usr/bin/varnish_reload_vcl'
+
       case $::operatingsystemmajrelease {
         '6': {
-          $repoclass       = "varnish::repo::el${::operatingsystemmajrelease}"
-          $sysconfig       = '/etc/sysconfig/varnish'
-          $varnish_version = '3.0'
+          $addrepo            = true
+          $repoclass          = "varnish::repo::el${::operatingsystemmajrelease}"
+          $sysconfig          = '/etc/sysconfig/varnish'
+          $varnish_version    = '3.0'
+          $sysconfig_template = 'varnish/el6/varnish-3.sysconfig.erb'
         }
         '7': {
-          $repoclass       = "varnish::repo::el${::operatingsystemmajrelease}"
-          $sysconfig       = '/etc/varnish/varnish.params'
-          $varnish_version = '4.0'
+          $addrepo            = true
+          $repoclass          = "varnish::repo::el${::operatingsystemmajrelease}"
+          $sysconfig          = '/etc/varnish/varnish.params'
+          $varnish_version    = '4.0'
+          $sysconfig_template = 'varnish/el7/varnish-4.sysconfig.erb'
         }
         default: {
           # Amazon Linux
-          $repoclass       = "varnish::repo::el${::operatingsystemmajrelease}"
-          $sysconfig       = '/etc/sysconfig/varnish'
-          $varnish_version = '3.0'
+          $addrepo            = true
+          $repoclass          = "varnish::repo::el${::operatingsystemmajrelease}"
+          $sysconfig          = '/etc/sysconfig/varnish'
+          $varnish_version    = '3.0'
+          $sysconfig_template = 'varnish/el6/varnish-3.sysconfig.erb'
+        }
+      }
+    }
+    'Debian': {
+      case $::lsbdistcodename {
+        'precise': {
+          $addrepo            = false
+          $sysconfig          = '/etc/default/varnish'
+          $varnish_version    = '3.0'
+          $sysconfig_template = 'varnish/debian/varnish-3.default.erb'
+          $vcl_reload         = '/usr/share/varnish/reload-vcl'
+        }
+        default: {
+          fail("${::lsbdistdescription} (${::lsbdistcodename}) not supported")
         }
       }
     }
     default: {
-      fail("${::operatingsystem} not supported")
+      fail("${::lsbdistdescription} (${::lsbdistcodename}) not supported")
     }
   }
 
@@ -48,3 +71,4 @@ class varnish::params {
   $storage_size   = '1G'
   $package_ensure = 'present'
 }
+
