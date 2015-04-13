076b096d48465f179a3d90124eb42037008de657
Doug Neal <doug@neal.me.uk>
2015-04-13 22:22:00 +0100

    Make specifying a secret optional and generate a UUID to use as the secret if one is not given. If the secretfile already contains a UUID, leave it alone.

--- manifests/secret.pp
+++ manifests/secret.pp
@@ -1,12 +1,25 @@
 # Specify the secret file for varnishadm
 # This file can be changed without notifying varnish
-class varnish::secret {
+class varnish::secret ($secret = undef) {
 
-  file { $varnish::params::secret_file:
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0600',
-    content => "${varnish::secret}\n",
+  if $secret {
+    file { $varnish::params::secret_file:
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0600',
+      content => "${secret}\n",
+    }
   }
+  else {
+    file { $varnish::params::secret_file:
+      owner => 'root',
+      group => 'root',
+      mode  => '0600',
+    }
 
+    exec { 'Generate Varnish secret file':
+      unless  => "/bin/egrep '^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$' '${varnish::params::secret_file}' >/dev/null",
+      command => "/bin/cp /proc/sys/kernel/random/uuid '${varnish::params::secret_file}'",
+    }
+  }
 }
