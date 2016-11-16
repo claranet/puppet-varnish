d953d84037cd32312ae8113592a561fc7735091e
Craig Watson <craig@bashton.com>
2016-11-16 09:53:43 +0000

    Fixing apt key errors

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -1,11 +1,14 @@
 # Add the Varnish repo
 class varnish::repo::debian {
     include apt
-    
+
     apt::source { 'varnish-cache':
         comment  => 'Apt source for Varnish 4',
         location => 'http://repo.varnish-cache.org/debian',
         repos    => "varnish-${::varnish::varnish_version}",
-        key      => 'E98C6BBBA1CBC5C3EB2DF21C60E7C096C4DEFFEB'
+        key      => {
+          source => 'https://repo.varnish-cache.org/GPG-key.txt',
+          id     => 'E98C6BBBA1CBC5C3EB2DF21C60E7C096C4DEFFEB',
+        },
     }
 }
