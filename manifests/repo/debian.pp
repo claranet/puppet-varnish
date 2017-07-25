b0403f4a11b2966beb9e6c89657c4d6bea89ed43
Mark Wilkinson <m.wilkinson@ctidigital.com>
2017-07-25 11:43:41 +0100

    ensure apt is updated after adding repo

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -3,12 +3,19 @@ class varnish::repo::debian {
     include ::apt
 
     ::apt::source { 'varnish-cache':
-        comment  => 'Apt source for Varnish 4',
+        comment  => "Apt source for Varnish ${::varnish::varnish_version}",
         location => 'http://repo.varnish-cache.org/debian',
         repos    => "varnish-${::varnish::varnish_version}",
         key      => {
           source => 'https://repo.varnish-cache.org/GPG-key.txt',
           id     => 'E98C6BBBA1CBC5C3EB2DF21C60E7C096C4DEFFEB',
         },
+    }
+
+    exec { 'varnish-cache apt-update':
+      command => 'apt-get update',
+      refreshonly => true,
+      subscribe   => Apt::Source['varnish-cache'],
+      path        => '/usr/bin:/usr/sbin',
     }
 }
