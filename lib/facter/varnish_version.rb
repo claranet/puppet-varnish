d67275234a7dc8d11147d9cee001cfe2d4e34690
Emerson Prado <emerson.prado@dataprev.gov.br>
2017-09-12 14:39:40 -0300

    Filter version info from exec outputs in Varnish version custom fact

--- lib/facter/varnish_version.rb
+++ lib/facter/varnish_version.rb
@@ -1,9 +1,9 @@
 Facter.add(:varnish_version) do
   setcode do
     if Facter::Core::Execution.which('varnishd')
-      Facter::Core::Execution.exec('varnishd -V 2>&1')
+      Facter::Core::Execution.exec('varnishd -V 2>&1')[/varnish-([\d\.]+)/, 1]
     else
-      Facter::Core::Execution.exec('yum list varnish')
+      Facter::Core::Execution.exec('yum list varnish')[/varnish\S*\s*([\d\.]+)/, 1]
     end
   end
 end
