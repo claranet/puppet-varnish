a049c7cd510822f6e9ed81a32800177bfaa83e5f
Emerson Prado <emerson.prado@dataprev.gov.br>
2017-09-12 14:38:21 -0300

    Include stderr from "varnishd -V" in Varnish version custom fact
    For insanity sake, "varnishd -V" sends version info to stderr

--- lib/facter/varnish_version.rb
+++ lib/facter/varnish_version.rb
@@ -1,7 +1,7 @@
 Facter.add(:varnish_version) do
   setcode do
     if Facter::Core::Execution.which('varnishd')
-      Facter::Core::Execution.exec('varnishd -V')
+      Facter::Core::Execution.exec('varnishd -V 2>&1')
     else
       Facter::Core::Execution.exec('yum list varnish')
     end
