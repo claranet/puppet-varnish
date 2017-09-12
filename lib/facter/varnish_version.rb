c2a4cfd18bf3d30f36c2e91ed5640d181884dd93
Emerson Prado <emerson.prado@dataprev.gov.br>
2017-09-12 17:29:14 -0300

    Document custom fact varnish_version

--- lib/facter/varnish_version.rb
+++ lib/facter/varnish_version.rb
@@ -1,8 +1,10 @@
 Facter.add(:varnish_version) do
   confine :osfamily => 'redhat'
   setcode do
+    # If executable is available, use it to query installed version
     if Facter::Core::Execution.which('varnishd')
       Facter::Core::Execution.exec('varnishd -V 2>&1')[/varnish-([\d\.]+)/, 1]
+    # Otherwise, pick first* version listed from Yum
     else
       Facter::Core::Execution.exec('yum list varnish')[/varnish\S*\s*([\d\.]+)/, 1]
     end
@@ -12,10 +14,15 @@ end
 Facter.add(:varnish_version) do
   confine :osfamily => 'debian'
   setcode do
+    # If executable is available, use it to query installed version
     if Facter::Core::Execution.which('varnishd')
       Facter::Core::Execution.exec('varnishd -V 2>&1')[/varnish-([\d\.]+)/, 1]
+    # Otherwise, pick first* version listed from Apt
     else
       Facter::Core::Execution.exec('apt-cache show varnish')[/Version:\s*([\d\.]+)/, 1]
     end
   end
 end
+
+# * I'm optimistically assuming the first listed version
+# is the highest one available
