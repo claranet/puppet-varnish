85b3f6c112a361572d2bd08fa13aca94f380116b
Emerson Prado <emerson.prado@dataprev.gov.br>
2017-09-12 15:39:19 -0300

    Include Debian family resolution for Varnish version custom fact

--- lib/facter/varnish_version.rb
+++ lib/facter/varnish_version.rb
@@ -8,3 +8,14 @@ Facter.add(:varnish_version) do
     end
   end
 end
+
+Facter.add(:varnish_version) do
+  confine :osfamily => 'debian'
+  setcode do
+    if Facter::Core::Execution.which('varnishd')
+      Facter::Core::Execution.exec('varnishd -V 2>&1')[/varnish-([\d\.]+)/, 1]
+    else
+      Facter::Core::Execution.exec('apt-cache show varnish')[/Version:\s*([\d\.]+)/, 1]
+    end
+  end
+end
