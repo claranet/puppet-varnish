7d54f34d3119b14accf210964c520d0d4ef63d6f
Larry Walton <lwalton@rsglab.com>
2019-01-28 14:22:41 -0800

    Adding ubuntu support

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -68,6 +68,8 @@ class varnish::repo {
       $os_lower        = downcase($::operatingsystem)
       $package_require = Exec['apt_update']
       $gpg_key_id      = "${::varnish::version_major}.${::varnish::version_minor}" ? {
+        '6.1' => '4A066C99B76A0F55A40E3E1E387EF1F5742D76CC',
+        '6.0' => '7C5B46721AF00FD57E68E6E8D2605BF74E8B9DBA',
         '5.2' => '91CFD5635A1A5FAC0662BEDD2E9BA3FE86BE909D',
         '5.1' => '54DC32329C37703D8B2819E6414C46826B880524',
         '5.0' => '1487779B0E6C440214F07945632B6ED0FF6A1C76',
